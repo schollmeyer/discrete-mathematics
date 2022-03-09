afb4ee59c3ce797a1e46f065a4c92aafdf8c2429
Georg Schollmeyer <69206011+schollmeyer@users.noreply.github.com>
2022-03-09 14:20:43 +0100

    Update VC_in_FCA.R

--- VC_in_FCA.R
+++ VC_in_FCA.R
@@ -1,5 +1,19 @@
 sample_shatterable_K_objset <- function(context,K,subset=rep(0,nrow(context){
-
+  if(sum(subset)==K){return(subset)}
+  extent <- operator_closure_obj_input(subset,context)
+  idx <- which(extent==0)
+  
+  for(k in sample(idx)){
+    new_subset <- subset
+    new_subset[k] <-1
+    if(objset_is_shatterable(new_subset,context){return(new_subset)}
+       
+    }
+    
+    return(NULL)
+    
+    
+    
 
 
 
