1443f4c75380f9dc29a3b72e6e27a7f3462d5a43
Georg Schollmeyer <69206011+schollmeyer@users.noreply.github.com>
2022-04-07 13:55:06 +0200

    Update statistical_models_for_partial_rankings.R

--- statistical_models_for_partial_rankings.R
+++ statistical_models_for_partial_rankings.R
@@ -233,7 +233,7 @@ nu_closed <- function(E,context){
   m <- sum(E)
   if(m==1){return(which(E==1))}
   if(m==1 | all(colSums(context[which(E==1),]) %in% c(0,m))){return(which(E==1)[1])}
-    extr <- extreme_points(context)
+    extr <- extreme_points(E,context)
     ans <- rep(0,nrow(context))
     ans[which(extr==1)] <- 1
     extr <- operator_closure_obj_input(extr,context)
