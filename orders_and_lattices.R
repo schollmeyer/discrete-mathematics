021e5f68efee9cd881883255cde414704c6ab281
Georg Schollmeyer <69206011+schollmeyer@users.noreply.github.com>
2022-09-30 01:47:44 +0200

    Update orders_and_lattices.R

--- orders_and_lattices.R
+++ orders_and_lattices.R
@@ -2354,9 +2354,7 @@ Tukeys_true_median_order <- function(orders){   ## coputes that partial order in
  while(TRUE){
     w <- max(W[which(ans_old==0)])
     i <- which(ans_old==0 & W==w)
-	i <- sample(rep(i,2),size=1)
-	i <<- i
-	print(w)
+    i <- sample(rep(i,2),size=1)	
     ans_new <- ans_old
     ans_new[i] <- 1
     if(! is_extendable_to_partial_order(ans_new)){
@@ -2381,4 +2379,21 @@ is_extendable_to_partial_order <- function( complemented_order ){
   if(any(M1==1 & M2 ==1)){return(FALSE)}
   if(!relation_is_acyclic(as.relation(M1))){return(FALSE)}
 return(TRUE)}
-						    
+						  
+test_Tukeys_true_median_order <- function(){
+	q <- 5
+	a <- all_partial_orders(q,complemented=FALSE)
+	orders <- list()
+	m <- nrow(a)
+	for(k in (1:m)){temp <- a[k,];dim(temp) <- c(q,q);orders[[k]] <- cbind(temp,1-temp)}
+	i <- sample((1:m),size=2000)
+	a <- cbind(a,1-a)
+	ans1 <- Tukeys_true_median_order(orders[i])
+	TD <- Tukeys_depth(rbind(a[i,],a),weights=c(rep(1,length(i)),rep(0,m)))
+	j <- which.max(TD)
+	ans2 <- (rbind(a[i,],a))[j,];dim(ans2) <- c(q,2*q)
+	ans1 <<- ans1
+	ans2 <<- ans2
+	return(all(ans1== ans2))
+}
+	
