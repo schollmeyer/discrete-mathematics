3b20b12ad5dd8dabfbc531db99a9beb4c24e2bdf
Georg Schollmeyer <69206011+schollmeyer@users.noreply.github.com>
2022-09-30 10:42:27 +0200

    Update orders_and_lattices.R

--- orders_and_lattices.R
+++ orders_and_lattices.R
@@ -2025,7 +2025,7 @@ cond.print=function(text,step,stepsize){if(stepsize != Inf){if(step %% stepsize 
 ##########################################
 						     
 						     
-starshaped_subgroup_discovery=  <- function(Z,u,params=list(Outputflag=0)){#x.train,y.train,x.test,y.test,stylizedBetweenness=sb1,p,VCDim,params=list(outputFlag=0,presolve=0,threads=1),VCcut=TRUE,interval){
+starshaped_subgroup_discovery  <- function(Z,u,params=list(Outputflag=0)){#x.train,y.train,x.test,y.test,stylizedBetweenness=sb1,p,VCDim,params=list(outputFlag=0,presolve=0,threads=1),VCcut=TRUE,interval){
   m <- dim(Z)[1]
   M <- list(modelsense="max",obj=u,lb=rep(0,m),ub=rep(1,m))
   solutions <- list()
