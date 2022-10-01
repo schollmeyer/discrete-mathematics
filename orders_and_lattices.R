6dab8a0b9b10be3b1cf55b80aa8723d046242881
Georg Schollmeyer <69206011+schollmeyer@users.noreply.github.com>
2022-10-01 23:30:59 +0200

    Update orders_and_lattices.R

--- orders_and_lattices.R
+++ orders_and_lattices.R
@@ -2449,7 +2449,7 @@ test_Tukeys_true_median_order <- function(){
 	TD <- Tukeys_depth(rbind(a[i,],a),weights=c(rep(1,length(i)),rep(0,m)))
 	j <- which(TD==max(TD))
 	for(jj in j){
-	ans2 <- (rbind(a[i,],a))[j,];dim(ans2) <- c(q,2*q)
+	ans2 <- (rbind(a[i,],a))[jj,];dim(ans2) <- c(q,2*q)
 	ans1 <<- ans1
 	ans2 <<- ans2
 	if(all(ans1==ans2)){return(TRUE)}
