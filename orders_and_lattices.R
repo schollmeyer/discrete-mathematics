02f91efe5327ff1eafe3d9f74f0177cacf84703f
Georg Schollmeyer <69206011+schollmeyer@users.noreply.github.com>
2022-10-01 22:58:24 +0200

    Update orders_and_lattices.R

--- orders_and_lattices.R
+++ orders_and_lattices.R
@@ -2413,7 +2413,7 @@ Tukeys_true_median_difference <- function(orders1,orders2){   ## coputes that pa
 
 Tukeys_geodetic_median_order <- function(corders, proportion){
 	context <- list_to_context(corders)
-	TD <- Tukeys_depth(corders)
+	TD <- Tukeys_depth(context)
 	i <- which(TD>=quantile(TD,1-proportion))
 	extent <- rep(0, length(corders))
 	intent <- calculate_psi(extent, context)
